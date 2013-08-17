0ed7274244cdbbb875b06a902869831000e7c296
aps-bhat <aps2891@gmail.com>
2013-08-17 13:33:27 +0530

    Delete register.py

--- neuron/register.py
+++
@@ -1,8 +0,0 @@
-class RegisterUser(object):
-  def __init__(self,request):
-     self.settings = request.registry.settings
-     self.collection = request.db['users']
-
-  def EnterUser(self, username, password, email):
-     user={'username':username, 'password':password, 'email_id':email }
-     self.collection.insert(user)
