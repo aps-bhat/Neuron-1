ac1f69cdb5104eb10cf0ed7a464428b133ef6bcb
aps-bhat <aps2891@gmail.com>
2013-08-17 13:37:04 +0530

    Delete registerprof.py

--- neuron/registerprof.py
+++
@@ -1,8 +0,0 @@
-class RegisterProfile(object):
-  def __init__(self,request):
-     self.settings = request.registry.settings
-     self.collection = request.db['profile']
-
-  def EnterProfDetails(self, username, fname, lname, date, month, year, city, designation):
-     profile={'username':username, 'fname':fname, 'email_id':email } #to be changed
-     self.collection.insert(user)
